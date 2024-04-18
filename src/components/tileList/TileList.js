c0e8b0f83ac04a01df37458cf24e91411fc30edd
Ben Zuniga <benzun3@gmail.com>
2024-04-17 23:33:12 -0400

    Tile List

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { Tile } from "../tile/Tile";
 
-export const TileList = () => {
+export const TileList = ({items}) => {
   return (
     <div>
-      
+        <ul>
+          {
+            items.map((item) =>
+              <li><Tile name={item.name} description={item.description}/></li>
+            )
+          }
+        </ul>
     </div>
   );
 };
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -45,7 +45,7 @@ export const ContactsPage = ({contacts, addContact}) => {
       <hr />
       <section>
         <h2>Contacts</h2>
-        <TileList contacts={contacts}/>
+        <TileList items={contacts}/>
       </section>
     </div>
   );
